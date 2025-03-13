66dd6b87a691b90a8ba72aabb2afdb34ccab6f09
Arlesson-Sales <urameshy11@gmail.com>
2025-03-13 14:44:28 -0300

    Ajustando os item da lista de lembretes, agora quando um novo item é adicionado ele recebe foco e quando é removido o anterior a ele recebe o focu

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import Style from './Item.module.css'
 
-export default function Item()
+/**
+ * Esse evento faz a verificação se o item de lista não possui mais um valor, ou sejá não possui
+ * mais nenhum texto, caso sim se o usuario apertar a tecla Backspace ele é removido.
+ * 
+ * @param {Object} event referencia ao objeto de evento.
+ */
+function removeItemCheck(event)
+{
+    if (event.key === "Backspace" && event.target.value === "")
+    {
+        const parentNode = event.target.parentNode.parentNode;
+        parentNode?.children?.[parentNode?.children.length - 2]?.lastChild?.focus?.();
+        event.target.parentNode.remove();
+    }
+}
+
+export default function Item({ ref })
 {
     return (<div className={Style.Item_list}>
         <input type="checkbox"/>
-        <input type="text" placeholder="Adicionar item" />
+        <input ref={ref} onKeyDown={removeItemCheck} type="text" placeholder="Adicionar item" />
     </div>)
 }
--- src/components/Textarea/Textarea.jsx
+++ src/components/Textarea/Textarea.jsx
@@ -1,25 +1,47 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import Style from './Textarea.module.css'
 import Condition from '../Condition/Condition'
 import Item from '../Item/Item'
 
+/**
+ * Essa evento é chamado sempre que algo é digitado na caixa de texto, ele é responsavel
+ * por atualizar o tamanho da caixa caso o texto digitado ultrapasse o limite.
+ * 
+ * @param {Object} event referencia ao objeto de evento.
+ */
 function textareaDinamicHeight(event)
 {
     event.target.style.height = "auto";
     event.target.style.height = String(event.target.scrollHeight) + "px";
 }
 
-function getItensElements(amount)
+/**
+ * Quando essa função é chamada ela cria uma vetor com componentes Item de acordo com a 
+ * quantidade passada como argumento na chamada dessa função.
+ * 
+ * @param {Number} amount quantidade de elementos Item.
+ * @param {Object} last_item_reference objeto gerado pelo hook useRef
+ * @returns 
+ */
+function getItensElements(amount, last_item_reference)
 {
     const list = [];
     for (let index = 0; index < amount; index++)
-        list.push(<Item key={index} />);
+    {
+        if (index === amount - 1)
+            list.push(<Item key={index} ref={last_item_reference} />);
+        else
+            list.push(<Item key={index} />);
+    }
     return list;
 }
 
 export default function Textzone()
 {
-    const [ itens_amount, setAmount ] = useState(0);
+    const [ itens_amount, setAmount ] = useState(() => 0);
+    const last_item_reference = useRef(null);
+
+    useEffect(() => last_item_reference.current?.focus() , [ itens_amount ]);
 
     return (<section className={Style.reminder_area}>
         <div className={Style.reminder_title}>
@@ -29,7 +51,9 @@ export default function Textzone()
             <textarea onInput={textareaDinamicHeight}></textarea>
             
             <div className="default_bar"></div>
-            {getItensElements(itens_amount)}
+            <div>
+                {getItensElements(itens_amount, last_item_reference)}
+            </div>
 
             <input onClick={() => setAmount(value => value + 1)} className="default_button" type="button" value={ itens_amount ? " +  Adicionar item" : "+  Adicionar lista verif." } />
         </div>
